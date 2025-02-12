cb1253e0501cabd7dd8ee9904e4d9e535483f38c
azharul <azharul@businessaccelerate.com.bd>
2025-02-12 14:19:32 +0600

    user and roles

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Role;
 use Illuminate\Http\Request;
 
-class RoleController extends Controller
-{
+class RoleController extends Controller{
     public function index(){
         return response()->json(Role::all()); // Get all roles
     }
--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,30 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RoleController;
 
-
-// routes/api.php
 Route::get('/test', function () {
     return response()->json(['message' => 'Test route is working']);
 });
 
-Route::group(['prefix' => 'auth'], function ($router) {
-	Route::post('/login', [AuthController::class, 'login']);
-	Route::post('/register', [AuthController::class, 'register']);
-
+// Auth Routes
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 });
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/users', [AuthController::class, 'users']);
-});
-
-Route::middleware('auth:api')->group(function () {
+    
+    // Roles Routes
     Route::get('/roles', [RoleController::class, 'index']);
     Route::post('/roles', [RoleController::class, 'store']);
     Route::get('/roles/{id}', [RoleController::class, 'show']);
     Route::put('/roles/{id}', [RoleController::class, 'update']);
     Route::delete('/roles/{id}', [RoleController::class, 'destroy']);
 });
+
 
